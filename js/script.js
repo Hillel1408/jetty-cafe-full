872f2cf1383e93e452d94adc6b0a788d73f6642f
Hillel1408 <etemax@bk.ru>
2023-05-05 12:10:47 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+});
+
+new fullpage('#fullpage', {
+    autoScrolling: true,
+});
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+Fancybox.bind('[data-fancybox="gallery"]', {});
+
+const func = (btn, block) => {
+    btn.addEventListener('click', function () {
+        this.classList.toggle('active');
+        if (block.style.maxHeight) block.style.maxHeight = null;
+        else block.style.maxHeight = block.scrollHeight + 'px';
+    });
+};
+
+const menu = document.querySelector('.menu__grid');
+const menuBtn = document.querySelector('.menu-btn');
+
+func(menuBtn, menu);
+
+const poster = document.querySelector('.poster__grid');
+const posterBtn = document.querySelector('.poster-btn');
+
+func(posterBtn, poster);
