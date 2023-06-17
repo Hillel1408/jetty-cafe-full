c31cbd937ccf46f86f7f63cf4740c817cca801f3
Hillel1408 <etemax@bk.ru>
2023-06-17 10:47:00 +0300

    jetty-cafe

--- #src/js/script.js
+++ #src/js/script.js
@@ -8,10 +8,27 @@ const swiper = new Swiper('.swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    loop: true,
     autoplay: {
         delay: 3000,
     },
+    loop: true,
+    speed: 1900,
+});
+const swiper2 = new Swiper('.swiper-2', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 3000,
+    },
+    effect: 'fade',
     speed: 1900,
 });
 
